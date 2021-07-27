60d7ec84580d19a34cc3c1fd0a1b29b92b075cb1
Rosario Pulella <RosarioPulella@gmail.com>
2021-07-27 18:47:40 -0400

    Make MapT a Beam table with PK id

--- backend/src/Backend/Mapper/Database/Map.hs
+++ backend/src/Backend/Mapper/Database/Map.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric, StandaloneDeriving, FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric, StandaloneDeriving, FlexibleInstances, TypeFamilies #-}
 module Backend.Mapper.Database.Map
   ( MapT(..)
   , Map
@@ -8,27 +8,35 @@ module Backend.Mapper.Database.Map
 import Prelude hiding (id)
 
 import Data.Text     (Text)
-import Database.Beam (C, Generic, PrimaryKey, Identity)
-{-
+import Database.Beam (C, Generic, PrimaryKey, Identity, Beamable, Table, primaryKey)
+{-|
+A Map table type.
+The Map table contains visual maps to place interactive Regions on. 
 NOTE: Width and Height might not be needed?
-
-CREATE TABLE Map (
-    id INT,
-    image DATA NOT NULL,
-    width REAL NOT NULL,
-    height REAL NOT NULL,
-    PRIMARY KEY (id),
-)
 -}
 data MapT f = Map
-    { id :: C f Int
-    , image :: C f Text
-    , width :: C f Double
-    , height :: C f Double
+    { id :: C f Int        -- ^ The Id of the map.
+    , image :: C f Text    -- ^ TODO: The data or path to the image to display for the map.
+    , width :: C f Double  -- ^ The width of the image.
+    , height :: C f Double -- ^ The height of the image.
     } deriving Generic
 
-type Map = MapT Identity
-type MapId = PrimaryKey MapT Identity
+instance Beamable MapT
 
+type Map = MapT Identity
 deriving instance Show Map
 deriving instance Eq Map
+
+type MapId = PrimaryKey MapT Identity
+deriving instance Show MapId
+deriving instance Eq MapId
+
+{-|
+MapT is a Beam Table
+Its primary key is just id.
+-}
+instance Table MapT where
+    data PrimaryKey MapT f = MapId (C f Int) deriving Generic
+    primaryKey = MapId . id
+
+instance Beamable (PrimaryKey MapT)
