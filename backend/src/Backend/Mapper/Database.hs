2d9e3f9eba144c9f07a80960684a36999471fd35
Rosario Pulella <RosarioPulella@gmail.com>
2021-07-29 23:39:37 -0400

    Write Region table as beam table

--- backend/src/Backend/Mapper/Database.hs
+++ backend/src/Backend/Mapper/Database.hs
@@ -2,11 +2,13 @@
 module Backend.Mapper.Database where
 
 import Backend.Mapper.Database.Map
+import Backend.Mapper.Database.Region
 
 import Database.Beam (Generic, Database, TableEntity, DatabaseSettings, defaultDbSettings)
 
 data MapperDb f = MapperDb
                 { maps :: f (TableEntity MapT)
+                , regions :: f (TableEntity RegionT)
                 } deriving Generic
 
 instance Database be MapperDb
---
+++ backend/src/Backend/Mapper/Database/Region.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveGeneric, StandaloneDeriving, FlexibleInstances, TypeFamilies #-}
+module Backend.Mapper.Database.Region
+  ( RegionT(..)
+  , Region
+  , RegionId
+  ) where
+
+import Prelude hiding (id)
+
+import Backend.Mapper.Database.Map (MapT)
+
+import Data.Text     (Text)
+import Database.Beam (C, Generic, PrimaryKey, Identity, Beamable, Table, primaryKey)
+{-
+Can be on a map.
+NOTE: Think on how we want to store the vector graphical representation of a Region,
+like do we have the location separately? or do we pull out other info from the path?
+-}
+
+data RegionT f = Region {
+      on_map :: PrimaryKey MapT f -- ^ The map this region is on.
+    , id :: C f Int               -- ^ the id of the region on the map.
+    , x :: C f Double             -- ^ The x position of the region.
+    , y :: C f Double             -- ^ The y position of the region.
+    , svg_path :: C f Text        -- ^ The svg path defining the shape of the region.
+    } deriving Generic
+
+instance Beamable RegionT
+
+type Region = RegionT Identity
+deriving instance Show Region
+deriving instance Eq Region
+
+type RegionId = PrimaryKey RegionT Identity
+deriving instance Show RegionId
+deriving instance Eq RegionId
+
+{-|
+RegionT is a Beam Table
+Its primary key is on_map + id.
+-}
+instance Table RegionT where
+  data PrimaryKey RegionT f = RegionId (PrimaryKey MapT f) (C f Int) deriving Generic
+  primaryKey = RegionId <$> on_map <*> id
+
+instance Beamable (PrimaryKey RegionT)
+
