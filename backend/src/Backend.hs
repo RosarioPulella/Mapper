1bb6932970588da5e91e43c5619c480cea278874
Rosario Pulella <RosarioPulella@gmail.com>
2021-09-16 23:26:41 -0400

    Use gargoyle and Beam autoMigrate to bootstrap db on backend_run

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,10 +1,23 @@
-module Backend where
+module Backend (backend) where
+
+import Data.Pool (withResource)
+
+import Database.Beam.Postgres (Connection, runBeamPostgres)
+
+import Gargoyle.PostgreSQL.Connect (withDb)
 
 import Common.Route
 import Obelisk.Backend
+import Backend.Mapper.Database (migration)
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> do
+    withDb "database" $ \pool -> withResource pool runMigrations
+    serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
   }
+
+runMigrations :: Connection -> IO ()
+runMigrations connection = 
+  runBeamPostgres connection migration
